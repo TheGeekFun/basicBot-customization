117c938c03fa083f02cb43df159ee877bc2c06d3
TheGeekFun <thegeekfun@gmail.com>
2015-07-15 14:01:00 +0200

    Update extension.js

--- extension.js
+++ extension.js
@@ -104,7 +104,7 @@
         welcome: true,
         opLink: null,
         rulesLink: null,
-        themeLink: From Rock to Jazz, from Blues to Pop, everything that could be in the Awesome Mix.
+        themeLink: From Rock to Jazz, from Blues to Pop, everything that could be in the Awesome Mix.,
         fbLink: null,
         youtubeLink: null,
         website: null,
